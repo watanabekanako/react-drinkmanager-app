9b68d90213c059d1e6a9c277c99c09553f9d4b06
kayahara mai <mai.kayahara@rakus-partners.co.jp>
2023-04-18 13:50:04 +0900

    パス修正

--- src/component/pages/Login.tsx
+++ src/component/pages/Login.tsx
@@ -1,9 +1,35 @@
-import { FC, memo } from 'react';
+import { FC, memo } from "react";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import { PrimaryButton } from "../button/Button";
 
 type Props = {};
 
 const Login: FC<Props> = memo((props) => {
-  return <h1>ログイン画面です。</h1>;
+  return (
+    <Container maxWidth="sm">
+      <h1>ログイン</h1>
+      <FormControl>
+        <TextField
+          id="outlined-basic"
+          label="メールアドレス"
+          variant="outlined"
+          placeholder="メールアドレスを入力してください"
+          fullWidth
+        />
+        <TextField
+          id="outlined-basic"
+          label="パスワード"
+          variant="outlined"
+          placeholder="8文字以上16文字以内"
+          fullWidth
+        />
+        <PrimaryButton children="ログイン" />
+      </FormControl>
+      <p>会員登録はこちら</p>
+    </Container>
+  );
 });
 
 export default Login;
--- src/component/pages/Register.tsx
+++ src/component/pages/Register.tsx
@@ -1,9 +1,39 @@
-import { FC, memo } from 'react';
+import { FC, memo } from "react";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import FormControl from "@mui/material/FormControl";
+import { PrimaryButton } from "../button/Button";
+import { SecondaryInput, PrimaryInput } from "../input/Input";
 
 type Props = {};
 
 const Register: FC<Props> = memo((props) => {
-  return <h1>登録画面です。</h1>;
+  return (
+    <Container maxWidth="sm">
+      <h1>会員登録</h1>
+      <FormControl>
+        <SecondaryInput label="社員ID" placeHolder="(例)0000" />
+        <Stack direction="row" spacing={2}>
+          <SecondaryInput label="姓" placeHolder="(例)ラクス" />
+          <SecondaryInput label="名" placeHolder="(例)太郎" />
+        </Stack>
+        <PrimaryInput
+          label="メールアドレス"
+          placeHolder="(例)example@example.com"
+        />
+        <PrimaryInput label="パスワード" placeHolder="8文字以上16文字以内" />
+        <span>✅8文字以上16文字以内</span>
+        <span>✅半角英字大文字、小文字、数字の3種類を1つ必ず使用</span>
+        <PrimaryInput
+          label="確認用パスワード"
+          placeHolder="8文字以上16文字以内"
+        />
+        <span>✅8文字以上16文字以内</span>
+        <span>✅半角英字大文字、小文字、数字の3種類を1つ必ず使用</span>
+        <PrimaryButton children="会員登録" />
+      </FormControl>
+    </Container>
+  );
 });
 
 export default Register;
