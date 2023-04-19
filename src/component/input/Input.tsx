6ed1b3ed58ca7a193b93a8ae545045b8449a648c
kayahara mai <mai.kayahara@rakus-partners.co.jp>
2023-04-19 17:47:14 +0900

    インプット修正、在庫履歴モック作成

--- src/component/input/Input.tsx
+++ src/component/input/Input.tsx
@@ -1,70 +1,16 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
-import InputAdornment from "@mui/material/InputAdornment";
-import IconButton from "@mui/material/IconButton";
 
-const PrimaryInput = ({
-  type,
-  label,
-  placeHolder,
-  helperText,
-  errorInput,
-  InputProps,
-  ...props
-}: any) => {
+const PrimaryInput = ({ ...props }: any) => {
   return (
     <>
-      <TextField
-        id="outlined-basic"
-        type={type}
-        label={label}
-        variant="outlined"
-        placeholder={placeHolder}
-        margin="normal"
-        helperText={helperText}
-        fullWidth
-        error={errorInput}
-        {...props}
-        InputProps={InputProps}
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton
-              aria-label="toggle password visibility"
-              // onClick={handleClickShowPassword}
-              // onMouseDown={handleMouseDownPassword}
-              edge="end"
-            >
-              {placeHolder === "パスワード" ? "👁" : "a"}
-            </IconButton>
-          </InputAdornment>
-        }
-      />
+      <TextField variant="outlined" margin="normal" fullWidth {...props} />
     </>
   );
 };
 
-const SecondaryInput = ({
-  type,
-  label,
-  placeHolder,
-  helperText,
-  errorInput,
-  ...props
-}: any) => {
-  return (
-    <TextField
-      id="outlined-basic"
-      type={type}
-      label={label}
-      variant="outlined"
-      placeholder={placeHolder}
-      margin="normal"
-      helperText={helperText}
-      error={errorInput}
-      sx={{ minWidth: 0, flexGrow: 1 }}
-      {...props}
-    />
-  );
+const SecondaryInput = ({ ...props }: any) => {
+  return <TextField variant="outlined" margin="normal" {...props} />;
 };
 
 export { PrimaryInput, SecondaryInput };
